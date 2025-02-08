15295b0599e8049161243c8b0eabdff208363d69
FreemanFeynman <131465819+FreemanFeynman@users.noreply.github.com>
2025-02-08 20:48:21 +0100

    Delete canvas.js

--- canvas.js
+++
@@ -1,7 +0,0 @@
-export const canvas = document.getElementById("gameCanvas");
-export const ctx = canvas.getContext("2d");
-
-export function setupCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
